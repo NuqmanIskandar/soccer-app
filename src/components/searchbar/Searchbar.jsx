405d552b0fd152c1897df2b4cf0d98e9914778fc
Nuqman Iskandar <129922158+NuqmanIskandar@users.noreply.github.com>
2026-06-05 20:10:59 +0800

    Finish searchbar components

---
+++ src/components/searchbar/Searchbar.jsx
@@ -0,0 +1,64 @@
+import styles from './Searchbar.module.css'
+import searchIcon from '../../assets/search-icon.png'
+import { useEffect, useState } from 'react'
+
+const CODES = ["PL", "PD", "BL1", "SA"]
+
+const Searchbar = () => {
+
+    const [isFocused, setIsFocused] = useState(false)
+    const [query, setQuery] = useState("")
+    const [allTeams, setAllTeams] = useState([])
+    const [results, setResults] = useState([])
+
+    // fetch once on mount
+    useEffect(() => {
+        async function loadTeams() {
+            const res = await Promise.all(
+                CODES.map(code =>
+                    fetch(`/api/v4/competitions/${code}/teams`, {
+                        headers: { "X-Auth-Token": import.meta.env.VITE_FOOTBALL_API_KEY }
+                    }).then(r => r.json())
+                )
+            )
+            setAllTeams(res.flatMap(r => r.teams))
+        }
+        loadTeams()
+    }, [])
+
+    // filter locally on query change
+    useEffect(() => {
+        if (!query) return setResults([])
+        const filtered = allTeams.filter(team =>
+            team.name.toLowerCase().includes(query.toLowerCase())
+        )
+        setResults(filtered.slice(0, 5))
+    }, [query, allTeams])
+
+    return(
+        <>
+            <div className={styles.searchContainer}>
+                <img src={searchIcon} className={styles.searchIcon}/>
+                <input
+                    className={styles.input}
+                    onChange={(e) => setQuery(e.target.value)}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setTimeout(() => setIsFocused(false), 200)}
+                    placeholder='search...'
+                />
+            </div>
+            <div className={styles.resultContainer}>
+                {isFocused && results.length > 0 && (
+                    results.map(team => (
+                        <button key={team.id} className={styles.dropdownItem}>
+                            <img src={team.crest} width={30}/>
+                            {team.name}
+                        </button>
+                    ))
+                )}
+            </div>
+        </>
+    )
+}
+
+export default Searchbar
--- src/pages/start/Start.jsx
+++ src/pages/start/Start.jsx
@@ -1,12 +1,17 @@
 import styles from './Start.module.css'
 import Navbar from '../../components/navbar/Navbar'
+import Searchbar from '../../components/searchbar/Searchbar'
 
 const Start = () => {
+
     return(
         <>
-            <div>
+            <div className={styles.wrapper}>
                 <Navbar/>
-                <h1>test</h1>
+                <div className={styles.searchWrapper}>
+                    <Searchbar/>
+                </div>
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, quidem repellendus, suscipit distinctio facilis soluta veniam, blanditiis laboriosam velit sequi praesentium cupiditate? Ut rem numquam, cumque tempore architecto at consequuntur.</h1>
             </div>
         </>
     )
